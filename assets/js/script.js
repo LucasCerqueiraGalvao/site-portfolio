c5fbccd43de5ef638cd187d54e7da15103415356
LucasCerqueiraGalvao <lucasgalvao134@gmail.com>
2024-07-17 11:53:32 -0300

    mudanças de estrutura do portifolio, apliquei o formato de timeline

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,3 +173,24 @@ portfolioLinks.forEach(link => {
   });
 });
 
+function copyToClipboard(text) {
+  // Create a temporary input element
+  var tempInput = document.createElement('input');
+  tempInput.value = text;
+  document.body.appendChild(tempInput);
+  
+  // Select the text in the input element
+  tempInput.select();
+  tempInput.setSelectionRange(0, 99999); // For mobile devices
+  
+  // Copy the text to the clipboard
+  document.execCommand('copy');
+  
+  // Remove the temporary input element
+  document.body.removeChild(tempInput);
+  
+  // Notify the user
+  alert('Email copied to clipboard: ' + text);
+}
+
+
